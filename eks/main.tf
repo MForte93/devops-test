cc92256a5d7688ac8f0a0865e18817f0d0706e26
Martin <mforte@tenaris.com>
2022-09-11 13:14:14 -0300

    update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -24,7 +24,7 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
-
+ 
 module "eks_cluster" {
   source = "github.com/brikis98/terraform-up-and-running-code//code/terraform/07-working-with-multiple-providers/modules/services/eks-cluster?ref=v0.3.0"
   name = "terraform-learning"
