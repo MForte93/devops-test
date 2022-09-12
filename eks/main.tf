932c1f051d709dc531cbf2e6c8cba0ab0d102ce9
Martin <mforte@tenaris.com>
2022-09-12 01:55:53 -0300

    Point backend

--- eks/main.tf
+++ eks/main.tf
@@ -28,7 +28,7 @@ provider "aws" {
 }
  
 module "eks_cluster" {
-  source = "./terraform/modules/services/eks-cluster"
+  source = "github.com/MForte93/devops-test//code/terraform/modules/services/eks-cluster"
   name = "terraform-learning"
   min_size     = 2
   max_size     = 2
