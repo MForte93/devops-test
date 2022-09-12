eb27497356843dc184806c908f79183ee6364989
Martin <mforte@tenaris.com>
2022-09-12 14:25:05 -0300

    Reversionado

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ def read_root():
     
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q} 
       
